b4e208bb385c6037a848b945106dad3a4f818f2e
Lizard-13 <thelizard-13@hotmail.xom>
2014-12-07 21:34:43 -0200

    Verificación de correcta eliminación de referencias de escena-capas-cámaras-fbos

--- LE/Ejemplos/ej_basico.py
+++ LE/Ejemplos/ej_basico.py
@@ -18,15 +18,26 @@ class EscenaBasica(le.Escena):
         # Eventos varios
         if self.eventos.entrada_otros[QUIT]:
             self.nucleo.salir()
-        
+
+# Se crea el núcleo
 nucleo = le.Nucleo()
+# Se crea una nueva escena, con lógica definida
 escena = EscenaBasica("LizardEngine - Ejemplo Básico")
+# Se establece la escena
 nucleo.cambiar_escena(escena)
+# Se crea una cámara y se definen sus framebuffers
 camara = le.Camara(tam=escena.tam)
 camara.fbos = [le.Framebuffer(escena.tam)]
+# Se establece la camara
 escena.capa_base.camaras = [camara]
 
+# Se definen los eventos disponibles
 nucleo.mapa_eve.definir_otro(QUIT)
 nucleo.mapa_eve.definir_teclas(K_ESCAPE, K_SPACE)
 
+# Se limpian referencias sin uso
+escena = None
+camara = None
+
+# Se ejecuta el bucle principal
 nucleo.ejecutar()
